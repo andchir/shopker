76707e342fd99b3ba0ee42b1c0cbeb16b6fc105a
andchir <andchir@gmail.com>
2017-12-17 21:50:13 +0300

    Test MongoDB connection

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -10,9 +10,33 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/login');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        // $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+
+    public function testMongoConnection()
+    {
+
+        $client = static::createClient();
+        $client->enableProfiler();
+        $container = $client->getContainer();
+
+        $m = $container->get('doctrine_mongodb.odm.default_connection');
+        $db = $m->selectDatabase($container->getParameter('mongodb_database'));
+        $userCollection = $db->createCollection('user');
+
+        $usersCount = 0;
+        try {
+            $usersCount = $userCollection->find()->count();
+
+        } catch (\MongoConnectionException $e) {
+            echo PHP_EOL . $e->getMessage();
+        }
+
+        $this->assertGreaterThanOrEqual(1, $usersCount);
+
+    }
+
 }

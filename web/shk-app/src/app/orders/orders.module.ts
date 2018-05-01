2476ff6d4a170d03f562c3ee02220734573ba372
andchir <andchir@gmail.com>
2018-05-01 13:27:36 +0300

    Split to modules. Lazy load modules

--- web/shk-app/src/app/orders/orders.module.ts
+++ web/shk-app/src/app/orders/orders.module.ts
@@ -1,10 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser';
-//import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {NgbModule, NgbActiveModal, NgbTooltipConfig} from '@ng-bootstrap/ng-bootstrap';
-import {EditorModule, CalendarModule, ChipsModule, ColorPickerModule, TreeModule} from 'primeng/primeng';
 
 import {SharedModule} from '../shared.module';
 import {OrdersService} from './orders.service';
@@ -14,36 +10,21 @@ import {ModalOrderContent, OrdersComponent} from './orders.component';
     imports: [
         CommonModule,
         SharedModule,
-        BrowserModule,
-        //BrowserAnimationsModule,
-        //FormsModule,
-        //ReactiveFormsModule,
-        HttpClientModule,
-        EditorModule,
-        CalendarModule,
-        ChipsModule,
-        ColorPickerModule,
-        TreeModule,
-        NgbModule.forRoot()
+        BrowserModule
     ],
     declarations: [
         OrdersComponent,
         ModalOrderContent
     ],
-
     providers: [
-        NgbActiveModal,
-        NgbTooltipConfig,
         OrdersService
     ],
-
     exports: [
 
     ],
     entryComponents: [
         ModalOrderContent
     ]
-
 })
 export class OrdersModule {
 }
--- web/shk-app/src/app/users/users.module.ts
+++ web/shk-app/src/app/users/users.module.ts
@@ -1,8 +1,5 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
-import {NgbModule, NgbActiveModal, NgbTooltipConfig} from '@ng-bootstrap/ng-bootstrap';
-import {EditorModule, CalendarModule, ChipsModule, ColorPickerModule, TreeModule} from 'primeng/primeng';
 
 import {SharedModule} from '../shared.module';
 import {UsersRoutingModule} from './users-routing.module';
@@ -13,33 +10,21 @@ import {ModalUserContent, UsersComponent} from './users.component';
     imports: [
         CommonModule,
         SharedModule,
-        HttpClientModule,
-        EditorModule,
-        CalendarModule,
-        ChipsModule,
-        ColorPickerModule,
-        TreeModule,
-        NgbModule.forRoot(),
         UsersRoutingModule
     ],
     declarations: [
         UsersComponent,
         ModalUserContent
     ],
-
     providers: [
-        NgbActiveModal,
-        NgbTooltipConfig,
         UsersService
     ],
-
     exports: [
 
     ],
     entryComponents: [
         ModalUserContent
     ]
-
 })
 export class UsersModule {
 }

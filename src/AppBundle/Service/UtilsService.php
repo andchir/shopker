9dea61aa755c4f43b9d9d43440e667f284dd2a0f
andchir <andchir@gmail.com>
2018-08-04 19:01:25 +0300

    Size format static method. Style - show-on-hover.

--- src/AppBundle/Service/UtilsService.php
+++ src/AppBundle/Service/UtilsService.php
@@ -277,4 +277,37 @@ class UtilsService
         return $result;
     }
 
+    /**
+     * @param $bytes
+     * @param string $unit
+     * @param int $decimals
+     * @return string
+     */
+    static function sizeFormat($bytes, $unit = "", $decimals = 2)
+    {
+        $units = array(
+            'B' => 0,
+            'KB' => 1,
+            'MB' => 2,
+            'GB' => 3,
+            'TB' => 4,
+            'PB' => 5,
+            'EB' => 6,
+            'ZB' => 7,
+            'YB' => 8
+        );
+        $value = 0;
+        if ($bytes > 0) {
+            if (!array_key_exists($unit, $units)) {
+                $pow = floor(log($bytes) / log(1024));
+                $unit = array_search($pow, $units);
+            }
+            $value = ($bytes / pow(1024, floor($units[$unit])));
+        }
+        if (!is_numeric($decimals) || $decimals < 0) {
+            $decimals = 2;
+        }
+        return sprintf('%.' . $decimals . 'f ' . $unit, $value);
+    }
+
 }

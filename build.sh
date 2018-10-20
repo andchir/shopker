7aad7f91e88d8fbc4d51da4d4d22a75377af4501
andchir <andchir@gmail.com>
2018-10-21 01:08:04 +0300

    Migrate to Symfony 4

--- build.sh
+++ build.sh
@@ -6,55 +6,57 @@ OUTPUT_FILENAME="shk4-latest.zip"
 
 [ -d "$DIR"/"$OUTPUT_DIRNAME" ] || mkdir "$DIR"/"$OUTPUT_DIRNAME"
 
+if [ -e "$DIR"/"$OUTPUT_FILENAME" ]
+then
+    rm "$DIR"/"$OUTPUT_FILENAME"
+fi
+
 rm -rfv "$DIR"/"$OUTPUT_DIRNAME"/*
 
 cp "$DIR"/composer.json "$DIR"/"$OUTPUT_DIRNAME"
 cp "$DIR"/LICENSE "$DIR"/"$OUTPUT_DIRNAME"
-cp -r "$DIR"/app "$DIR"/"$OUTPUT_DIRNAME"
+cp "$DIR"/.env "$DIR"/"$OUTPUT_DIRNAME"
 cp -r "$DIR"/bin "$DIR"/"$OUTPUT_DIRNAME"
+cp -r "$DIR"/config "$DIR"/"$OUTPUT_DIRNAME"
 cp -r "$DIR"/src "$DIR"/"$OUTPUT_DIRNAME"
+cp -r "$DIR"/templates "$DIR"/"$OUTPUT_DIRNAME"
 cp -r "$DIR"/tests "$DIR"/"$OUTPUT_DIRNAME"
+cp -r "$DIR"/translations "$DIR"/"$OUTPUT_DIRNAME"
 cp -r "$DIR"/vendor "$DIR"/"$OUTPUT_DIRNAME"
 
-rm "$DIR"/"$OUTPUT_DIRNAME"/app/config/settings.yml
-touch "$DIR"/"$OUTPUT_DIRNAME"/app/config/settings.yml
+rm "$DIR"/"$OUTPUT_DIRNAME"/config/settings.yaml
+touch "$DIR"/"$OUTPUT_DIRNAME"/config/settings.yaml
 
 mkdir "$DIR"/"$OUTPUT_DIRNAME"/var
 mkdir "$DIR"/"$OUTPUT_DIRNAME"/var/cache
-mkdir "$DIR"/"$OUTPUT_DIRNAME"/var/logs
-mkdir "$DIR"/"$OUTPUT_DIRNAME"/var/sessions
-cp "$DIR"/var/bootstrap.php.cache "$DIR"/"$OUTPUT_DIRNAME"/var
-cp "$DIR"/var/SymfonyRequirements.php "$DIR"/"$OUTPUT_DIRNAME"/var
+mkdir "$DIR"/"$OUTPUT_DIRNAME"/var/log
 
-mkdir "$DIR"/"$OUTPUT_DIRNAME"/web
-cp "$DIR"/web/*.* "$DIR"/"$OUTPUT_DIRNAME"/web
-rm "$DIR"/"$OUTPUT_DIRNAME"/web/package-lock.json
-cp -r "$DIR"/web/app_build "$DIR"/"$OUTPUT_DIRNAME"/web
-cp -r "$DIR"/web/css "$DIR"/"$OUTPUT_DIRNAME"/web
-cp -r "$DIR"/web/img "$DIR"/"$OUTPUT_DIRNAME"/web
-cp -r "$DIR"/web/js "$DIR"/"$OUTPUT_DIRNAME"/web
-mkdir "$DIR"/"$OUTPUT_DIRNAME"/web/uploads
+mkdir "$DIR"/"$OUTPUT_DIRNAME"/public
+cp "$DIR"/public/*.* "$DIR"/"$OUTPUT_DIRNAME"/public
+rm "$DIR"/"$OUTPUT_DIRNAME"/public/package-lock.json
+cp -r "$DIR"/public/app_build "$DIR"/"$OUTPUT_DIRNAME"/public
+cp -r "$DIR"/public/css "$DIR"/"$OUTPUT_DIRNAME"/public
+cp -r "$DIR"/public/img "$DIR"/"$OUTPUT_DIRNAME"/public
+cp -r "$DIR"/public/js "$DIR"/"$OUTPUT_DIRNAME"/public
+mkdir "$DIR"/"$OUTPUT_DIRNAME"/public/uploads
 
-mkdir "$DIR"/"$OUTPUT_DIRNAME"/web/node_modules
-cp -r "$DIR"/web/node_modules/bootstrap "$DIR"/"$OUTPUT_DIRNAME"/web/node_modules
-cp -r "$DIR"/web/node_modules/jquery "$DIR"/"$OUTPUT_DIRNAME"/web/node_modules
-cp -r "$DIR"/web/node_modules/nouislider "$DIR"/"$OUTPUT_DIRNAME"/web/node_modules
-cp -r "$DIR"/web/node_modules/popper.js "$DIR"/"$OUTPUT_DIRNAME"/web/node_modules
-cp -r "$DIR"/web/node_modules/wnumb "$DIR"/"$OUTPUT_DIRNAME"/web/node_modules
+cp -r "$DIR"/public/admin "$DIR"/"$OUTPUT_DIRNAME"/public/admin
+rm -rf "$DIR"/"$OUTPUT_DIRNAME"/public/admin/bundle-dev
 
-mkdir "$DIR"/"$OUTPUT_DIRNAME"/web/shk-app
-cp "$DIR"/web/shk-app/*.* "$DIR"/"$OUTPUT_DIRNAME"/web/shk-app
-cp -r "$DIR"/web/shk-app/bundle "$DIR"/"$OUTPUT_DIRNAME"/web/shk-app
-cp -r "$DIR"/web/shk-app/css "$DIR"/"$OUTPUT_DIRNAME"/web/shk-app
-cp -r "$DIR"/web/shk-app/e2e "$DIR"/"$OUTPUT_DIRNAME"/web/shk-app
-cp -r "$DIR"/web/shk-app/i18n "$DIR"/"$OUTPUT_DIRNAME"/web/shk-app
-cp -r "$DIR"/web/shk-app/img "$DIR"/"$OUTPUT_DIRNAME"/web/shk-app
-cp -r "$DIR"/web/shk-app/locale "$DIR"/"$OUTPUT_DIRNAME"/web/shk-app
-cp -r "$DIR"/web/shk-app/src "$DIR"/"$OUTPUT_DIRNAME"/web/shk-app
+mkdir "$DIR"/"$OUTPUT_DIRNAME"/public/node_modules
+cp -r "$DIR"/public/node_modules/bootstrap "$DIR"/"$OUTPUT_DIRNAME"/public/node_modules
+cp -r "$DIR"/public/node_modules/jquery "$DIR"/"$OUTPUT_DIRNAME"/public/node_modules
+cp -r "$DIR"/public/node_modules/nouislider "$DIR"/"$OUTPUT_DIRNAME"/public/node_modules
+cp -r "$DIR"/public/node_modules/popper.js "$DIR"/"$OUTPUT_DIRNAME"/public/node_modules
+cp -r "$DIR"/public/node_modules/wnumb "$DIR"/"$OUTPUT_DIRNAME"/public/node_modules
 
-mkdir "$DIR"/"$OUTPUT_DIRNAME"/web/shk-app/node_modules
-cp -r "$DIR"/web/shk-app/node_modules/bootstrap "$DIR"/"$OUTPUT_DIRNAME"/web/shk-app/node_modules
-cp -r "$DIR"/web/shk-app/node_modules/primeng "$DIR"/"$OUTPUT_DIRNAME"/web/shk-app/node_modules
+mkdir "$DIR"/"$OUTPUT_DIRNAME"/frontend
+cp "$DIR"/frontend/*.* "$DIR"/"$OUTPUT_DIRNAME"/frontend
+cp -r "$DIR"/frontend/css "$DIR"/"$OUTPUT_DIRNAME"/frontend
+cp -r "$DIR"/frontend/e2e "$DIR"/"$OUTPUT_DIRNAME"/frontend
+cp -r "$DIR"/frontend/img "$DIR"/"$OUTPUT_DIRNAME"/frontend
+cp -r "$DIR"/frontend/locale "$DIR"/"$OUTPUT_DIRNAME"/frontend
+cp -r "$DIR"/frontend/src "$DIR"/"$OUTPUT_DIRNAME"/frontend
 
-cd "$DIR"/"$OUTPUT_DIRNAME"; zip -r -9 ../"$OUTPUT_FILENAME" *
+cd "$DIR"/"$OUTPUT_DIRNAME"; zip -r -9 ../"$OUTPUT_FILENAME" .
 rm -rfv "$DIR"/"$OUTPUT_DIRNAME"/*
---
+++ config/bundles.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    App\MainBundle\AppMainBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
+    Andchir\OmnipayBundle\OmnipayBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+];

5052bf4febdefe7e17f8ca5e9e93d982acb7fe6a
andchir <andchir@gmail.com>
2018-11-04 14:03:37 +0300

    settings_script - prevent caching

--- src/App/Controller/Admin/HomepageController.php
+++ src/App/Controller/Admin/HomepageController.php
@@ -73,6 +73,9 @@ class HomepageController extends Controller
 
         $response = new Response($content);
         $response->headers->set('Content-Type', 'application/javascript');
+        $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+        $response->headers->set('Cache-Control', 'post-check=0, pre-check=0', false);
+        $response->headers->set('Pragma', 'no-cache');
 
         return $response;
     }

c6854c69ff1eff6d02eb808743e851f64df75268
andchir <andchir@gmail.com>
2018-12-14 15:20:16 +0300

    Build settings update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,13 +2,9 @@ import {NgModule} from '@angular/core';
 import {NoPreloading, Router, RouterModule, Routes} from '@angular/router';
 import {NotFoundComponent} from "./not-found.component";
 
+declare const adminRoutes: Routes;
+
 export const routes: Routes = [
-    {path: '', redirectTo: 'orders', pathMatch: 'full'},
-    {path: 'orders', loadChildren: './orders/orders.module#OrdersModule'},
-    {path: 'catalog', loadChildren: './catalog/catalog.module#CatalogModule'},
-    {path: 'statistics', loadChildren: './statistics/statistics.module#StatisticsModule'},
-    {path: 'users', loadChildren: './users/users.module#UsersModule'},
-    {path: 'settings', loadChildren: './settings/settings.module#SettingsModule'},
     {path: '**', component: NotFoundComponent}
 ];
 
@@ -20,5 +16,11 @@ export const routes: Routes = [
     exports: [RouterModule]
 })
 export class AppRoutingModule {
+
+    constructor (
+        private router: Router
+    ) {
+        this.router.config.unshift(...adminRoutes);
+    }
 
 }

549e8a15691078a7a9bd6f1b1cf48ba74dc1176f
andchir <andchir@gmail.com>
2018-12-15 18:56:37 +0300

    rc-5

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
-import {NoPreloading, Router, RouterModule, Routes} from '@angular/router';
-import {NotFoundComponent} from "./not-found.component";
-
-declare const adminRoutes: Routes;
+import {NoPreloading, RouterModule, Routes} from '@angular/router';
+import {NotFoundComponent} from './not-found.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'orders', pathMatch: 'full'},
+    {path: 'orders', loadChildren: './orders/orders.module#OrdersModule'},
+    {path: 'catalog', loadChildren: './catalog/catalog.module#CatalogModule'},
+    {path: 'statistics', loadChildren: './statistics/statistics.module#StatisticsModule'},
+    {path: 'users', loadChildren: './users/users.module#UsersModule'},
+    {path: 'settings', loadChildren: './settings/settings.module#SettingsModule'},
     {path: '**', component: NotFoundComponent}
 ];
 
@@ -16,11 +20,5 @@ export const routes: Routes = [
     exports: [RouterModule]
 })
 export class AppRoutingModule {
-
-    constructor (
-        private router: Router
-    ) {
-        this.router.config.unshift(...adminRoutes);
-    }
 
 }

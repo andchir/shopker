0467db1766e8cd5453ed778ee288bf28e267888c
andchir <andchir@gmail.com>
2018-12-23 13:15:01 +0300

    Translation for contentList function

--- src/App/DataFixtures/MongoDB/en/CatalogFixtures.php
+++ src/App/DataFixtures/MongoDB/en/CatalogFixtures.php
@@ -822,7 +822,7 @@ class CatalogFixtures extends Fixture implements ContainerAwareInterface
                         'description' => '',
                         'form_name' => 'contacts',
                         'menuIndex' => 3,
-                        'translations' => ['title' => ['ru' => 'Контакты']]
+                        'translations' => ['title' => ['ru' => 'Контакты'], 'form_name' => ['en' => 'contacts']]
                     ]
                 ]
             ]

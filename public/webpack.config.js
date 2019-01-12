31ccf8fccda7c84b86ec43c4096b5e7c795de6c8
andchir <andchir@gmail.com>
2019-01-12 11:38:39 +0300

    Update components

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -14,6 +14,7 @@ Encore
     ])
     .cleanupOutputBeforeBuild()
     .autoProvidejQuery()
-    .enableSourceMaps(!Encore.isProduction());
+    .enableSourceMaps(!Encore.isProduction())
+    .disableSingleRuntimeChunk();
 
 module.exports = Encore.getWebpackConfig();

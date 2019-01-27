4f670188247e16e4958d8fa6ccb8634ebf1bb232
andchir <andchir@gmail.com>
2019-01-27 17:20:14 +0300

    small fix

--- src/App/Form/Type/UserType.php
+++ src/App/Form/Type/UserType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\MainBundle\Document\User;
+use Symfony\Component\Validator\Constraints\Length;
 
 class UserType extends AbstractType
 {
@@ -16,6 +17,9 @@ class UserType extends AbstractType
     {
         $builder->add('email', EmailType::class);
         $builder->add('password', RepeatedType::class, [
+            'constraints' => [
+                new Length(['min' => 6, 'max' => 25])
+            ],
             'first_name' => 'password',
             'second_name' => 'confirm',
             'type' => PasswordType::class,

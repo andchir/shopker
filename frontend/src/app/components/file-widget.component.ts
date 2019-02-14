d6dd5b511f1574f3a7265d6ae84941214f634aea
andchir <andchir@gmail.com>
2019-02-14 14:49:00 +0300

    File widget update

--- frontend/src/app/components/file-widget.component.ts
+++ frontend/src/app/components/file-widget.component.ts
@@ -4,28 +4,32 @@ import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 import {FileData} from '../catalog/models/file-data.model';
 import {AppSettings} from '../services/app-settings.service';
 
+export const EPANDED_TEXTAREA_VALUE_ACCESSOR : any = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => FileWidgetComponent),
+    multi: true,
+};
+
 @Component({
     selector: 'app-file-widget',
     templateUrl: 'templates/app-file-widget.html',
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => FileWidgetComponent),
-            multi: true
-        }
-    ]
+    providers: [EPANDED_TEXTAREA_VALUE_ACCESSOR]
 })
-export class FileWidgetComponent implements OnInit, ControlValueAccessor {
+export class FileWidgetComponent implements ControlValueAccessor {
 
-    @Input() fieldName: string;
+    @Input() fieldName: string = 'image';
     @Input() fieldTitle: string;
     @Input() hasPreviewImage = false;
-    @Input() required = false;
     @Input() allowedExtensions = '';
     @Input() files: { [key: string]: File } = {};
     @ViewChild('imgPreview') imgPreview: ElementRef;
     @Input('controlValue') _controlValue: FileData|null = null;
     filesDirBaseUrl: string;
+    fileName = '';
+    imageUrl: string|ArrayBuffer = '';
+
+    _onChange: (value: FileData) => void = () => null;
+    _onTouched: () => void = () => null;
 
     constructor(
         private appSettings: AppSettings
@@ -39,15 +43,7 @@ export class FileWidgetComponent implements OnInit, ControlValueAccessor {
 
     set controlValue(val: FileData) {
         this._controlValue = val;
-        this.onChange(val);
-    }
-
-    onChange: (value: FileData) => void = () => null;
-
-    ngOnInit(): void {
-        if (!this.fieldName) {
-            this.fieldName = 'image';
-        }
+        this._onChange(val);
     }
 
     fileChange(event): void {
@@ -59,27 +55,27 @@ export class FileWidgetComponent implements OnInit, ControlValueAccessor {
     onGetFile(file: File): void {
         const value: FileData = FileData.getFileData(file);
         this.files[this.fieldName] = file;
-        this.writeValue(value);
         if (this.hasPreviewImage && file.type.indexOf('image/') > -1) {
             this.imgPreview.nativeElement.style.display = 'block';
             const reader = new FileReader();
             reader.onload = (e: ProgressEvent) => {
                 const fr = e.target as FileReader;
                 value.dataUrl = fr.result;
-                this.controlValue = value;
+                this.writeValue(value);
             };
             reader.readAsDataURL(file);
         } else {
             this.imgPreview.nativeElement.style.display = 'none';
+            this.writeValue(value);
         }
     }
 
-    getImageUrl(): string|ArrayBuffer {
-        return FileData.getImageUrl(this.filesDirBaseUrl, this.controlValue);
+    getImageUrl(data?: FileData|null): string|ArrayBuffer {
+        return FileData.getImageUrl(this.filesDirBaseUrl, data || this.controlValue);
     }
 
     fileClear() {
-        this.writeValue(null);
+        this.writeValue(null, true);
         delete this.files[this.fieldName];
         if (this.imgPreview && this.imgPreview.nativeElement) {
             this.imgPreview.nativeElement.src = '';
@@ -102,14 +98,28 @@ export class FileWidgetComponent implements OnInit, ControlValueAccessor {
         event.preventDefault();
     }
 
-    writeValue(value: FileData|null) {
-        this.controlValue = value;
+    writeValue(data: FileData|null, clearValue = false) {
+        if (!data) {
+            this.fileName = '';
+            this.imageUrl = '';
+            if (clearValue) {
+                this.controlValue = null;
+            }
+        } else {
+            this.fileName = `${data.title}.${data.extension}`;
+            this.imageUrl = this.getImageUrl(data);
+            this.controlValue = data;
+        }
     }
 
-    registerOnChange(fn: (_: FileData) => void): void {
-        this.onChange = fn;
+    registerOnChange(fn: (_: any) => void): void {
+        this._onChange = () => {
+            fn(this.controlValue);
+        }
     }
 
-    registerOnTouched() {}
+    registerOnTouched(fn: any): void {
+        this._onTouched = fn;
+    }
 
 }

9cf5628feabbcc5dd4299814fe6b751a0b134abf
Andrew Ch <andchir@gmail.com>
2020-12-24 13:07:44 +0300

    Carousel fix

--- public/js/app.js
+++ public/js/app.js
@@ -26,38 +26,41 @@ $(document).ready(function() {
         trigger: 'hover',
         placement: 'bottom'
     });
-    
-    $('.responsive-carousel').slick({
-        dots: true,
-        infinite: false,
-        arrows: false,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
+
+    $('.responsive-carousel').each(function(index, el) {
+        if ($(el).children().length > 4) {
+            $(el).slick({
+                dots: true,
+                infinite: false,
+                arrows: false,
+                speed: 300,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    }
+                ]
+            });
+        }
     });
-    
 });
 

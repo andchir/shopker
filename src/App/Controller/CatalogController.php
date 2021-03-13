66c741bf3cb8edbece8cd68e93dceb811ba75a28
Andrew Ch <andchir@gmail.com>
2021-03-14 02:25:01 +0300

    Filter by number update

--- src/App/Controller/CatalogController.php
+++ src/App/Controller/CatalogController.php
@@ -745,7 +745,7 @@ class CatalogController extends BaseController
             return;
         }
         foreach ($filters as $name => $filter) {
-            if (empty($filter)) {
+            if ($filter === '') {
                 continue;
             }
             if (!is_array($filter)) {

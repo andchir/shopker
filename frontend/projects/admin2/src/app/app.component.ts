603a5ff537b84d8b3367afade2e6cadf43ff2608
andrew <andchir@gmail.com>
2022-01-04 00:58:42 +0300

    new admin area - orders module

--- frontend/projects/admin2/src/app/app.component.ts
+++ frontend/projects/admin2/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 
 import {TranslateService} from '@ngx-translate/core';
+import {PrimeNGConfig} from 'primeng/api';
 
 import {AppSettings} from '@app/services/app-settings.service';
 
@@ -12,10 +13,13 @@ export class AppComponent {
 
     constructor(
         private translate: TranslateService,
-        private appSettings: AppSettings
+        private appSettings: AppSettings,
+        private primengConfig: PrimeNGConfig
     ) {
         this.translate.addLangs(['en', 'ru']);
         this.translate.setDefaultLang('en');
         this.translate.use(this.appSettings.settings.locale);
+
+        this.primengConfig.ripple = true;
     }
 }

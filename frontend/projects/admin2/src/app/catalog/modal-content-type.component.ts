e132dc244b13ed56b92345b6181c7021fb0d4634
andrew <andchir@gmail.com>
2022-02-20 23:30:54 +0300

    admin2 - Content type modal update

--- frontend/projects/admin2/src/app/catalog/modal-content-type.component.ts
+++ frontend/projects/admin2/src/app/catalog/modal-content-type.component.ts
@@ -4,14 +4,14 @@ import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 import {DynamicDialogConfig, DynamicDialogRef} from 'primeng/dynamicdialog';
 
 import {AppModalAbstractComponent} from '../components/modal.component.abstract';
-import {FormFieldsData} from '../models/form-field.interface';
 import {ContentType} from './models/content_type.model';
 import {ContentTypesService} from './services/content_types.service';
+import {CollectionsService} from './services/collections.service';
 
 @Component({
     selector: 'app-modal-content-type',
     templateUrl: 'templates/modal-content-type.component.html',
-    providers: []
+    providers: [CollectionsService]
 })
 export class ModalContentTypeComponent extends AppModalAbstractComponent<ContentType> implements OnInit, OnDestroy {
 
@@ -21,30 +21,41 @@ export class ModalContentTypeComponent extends AppModalAbstractComponent<Content
         title: new FormControl('', [Validators.required]),
         name: new FormControl('', [Validators.required, Validators.pattern('[A-Za-z0-9_-]+')]),
         description: new FormControl('', []),
-        isActive: new FormControl('', [])
+        collection: new FormControl('', [Validators.required]),
+        isActive: new FormControl('', []),
+        isCreateByUsersAllowed: new FormControl('', []),
     });
-    arrayFieldsData: {[key: string]: FormFieldsData} = {
-        inputProperties: {
-            name: {validators: [Validators.required]},
-            title: {validators: [Validators.required]},
-            default_value: {validators: []}
-        },
-        outputProperties: {
-            name: {validators: [Validators.required]},
-            title: {validators: [Validators.required]},
-            default_value: {validators: []}
-        }
-    };
+    collections: string[] = ['products'];
 
     constructor(
         public ref: DynamicDialogRef,
         public config: DynamicDialogConfig,
-        public dataService: ContentTypesService
+        public dataService: ContentTypesService,
+        private collectionsService: CollectionsService
     ) {
         super(ref, config, dataService);
     }
 
     ngOnInit() {
         super.ngOnInit();
+        this.getCollectionsList();
+    }
+
+    getCollectionsList(): void {
+        this.collectionsService.getList()
+            .subscribe(data => {
+                    if (data.length > 0) {
+                        this.collections = data;
+                    }
+                }
+            );
+    }
+
+    addCollectionToggle(newCollectionField: HTMLInputElement, event?: MouseEvent): void {
+        console.log('addCollectionToggle', newCollectionField);
+    }
+
+    deleteCollection(): void {
+        console.log('deleteCollection');
     }
 }

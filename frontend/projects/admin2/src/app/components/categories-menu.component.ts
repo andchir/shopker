a490300769665422302c2c8bdd4e899931c1e2ea
andrew <andchir@gmail.com>
2022-04-02 23:34:50 +0300

    admin2 - catalog categories menu

--- frontend/projects/admin2/src/app/components/categories-menu.component.ts
+++ frontend/projects/admin2/src/app/components/categories-menu.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, Input, Output, EventEmitter, OnDestroy} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 
-import {take, takeUntil} from 'rxjs/operators';
+import {takeUntil} from 'rxjs/operators';
 import {Subject} from 'rxjs';
 import {TranslateService} from '@ngx-translate/core';
 

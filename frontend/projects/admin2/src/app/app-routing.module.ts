cfc2dacca2a6a2aedb49fcb8a7ddc31105c0943d
andrew <andchir@gmail.com>
2023-01-06 20:15:10 +0300

    js build

--- frontend/projects/admin2/src/app/app-routing.module.ts
+++ frontend/projects/admin2/src/app/app-routing.module.ts
@@ -14,11 +14,12 @@ const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule)},
     {path: 'catalog', loadChildren: () => import('./catalog/catalog.module').then(m => m.CatalogModule)},
-    {path: 'import-export', loadChildren: () => import('./import-export/import-export.module').then(m => m.ImportExportModule)},
     {path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule)},
     {path: 'statistics', loadChildren: () => import('./statistics/statistics.module').then(m => m.StatisticsModule)},
     {path: 'code_edit', loadChildren: () => import('./code-edit/templates-edit.module').then(m => m.TemplatesEditModule)},
     {path: 'settings', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule)},
+    {path: 'import-export', loadChildren: () => import('./import-export/import-export.module').then(m => m.ImportExportModule)},
+    {path: 'comments', loadChildren: () => import('./comments/app.module').then(m => m.AppModule)},
     {path: 'module/:moduleName', component: ModuleLoaderComponent},
     {path: '**', component: NotFoundComponent}
 ];
